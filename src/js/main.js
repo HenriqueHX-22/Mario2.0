3db3d3a04408b73bf0856bda9a7365e3018229fc
Henrique <henrique.balzanelo@escola.pr.gov.br>
2025-08-12 09:37:51 -0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,103 +1,95 @@
-const mario = document.querySelector('.mario')
-const pipe = document.querySelector('.pipe')
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const start = document.querySelector('.start');
+const gameOver = document.querySelector('.game-over');
 
-const start = document.querySelector('.start')
-const gameOver = document.querySelector('.game-over')
+const audioStart = new Audio('./src/audio/audio_theme.mp3');
+const audioGameOver = new Audio('./src/audio/audio_gameover.mp3');
 
-audioStart = new Audio('./src/audio/audio_theme.mp3')
-audioGameOver = new Audio('./src/audio/audio_gameover.mp3')
-
+let gameLoop; // Variable to store the interval ID
 
 const startGame = () => {
-  pipe.classList.add('pipe-animation')
-  start.style.display = 'none'
+  pipe.classList.add('pipe-animation');
+  start.style.display = 'none';
 
   // audio
-  audioStart.play()
-}
+  if (!audioStart.paused) {
+    audioStart.currentTime = 0; // Restart the audio
+  }
+  audioStart.play();
+};
 
 const restartGame = () => {
-  gameOver.style.display = 'none'
-  pipe.style.left = ''
-  pipe.style.right = '0'
-  mario.src = './src/img/mario.gif'
-  mario.style.width = '150px'
-  mario.style.bottom = '0'
+  gameOver.style.display = 'none';
+  pipe.style.left = '';
+  pipe.style.right = '0';
+  mario.src = './src/img/mario.gif';
+  mario.style.width = '150px';
+  mario.style.bottom = '0';
 
-  start.style.display = 'none'
+  start.style.display = 'none';
 
-  audioGameOver.pause()
+  audioGameOver.pause();
   audioGameOver.currentTime = 0;
 
-  audioStart.play()
-  audioStart.currentTime = 0;
-
-}
+  if (!audioStart.paused) {
+    audioStart.currentTime = 0;
+  }
+  audioStart.play();
+};
 
 const jump = () => {
-  mario.classList.add('jump')
+  mario.classList.add('jump');
 
   setTimeout(() => {
-    mario.classList.remove('jump')
-  }, 800)
-}
+    mario.classList.remove('jump');
+  }, 800);
+};
 
 const loop = () => {
-  setInterval(() => {
-    const pipePosition = pipe.offsetLeft
-    const marioPosition = window
-      .getComputedStyle(mario)
-      .bottom.replace('px', ' ')
+  gameLoop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = parseInt(window.getComputedStyle(mario).bottom.replace('px', ''));
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-      pipe.classList.remove('.pipe-animation')
-      pipe.style.left = `${pipePosition}px`
+      pipe.classList.remove('pipe-animation');
+      pipe.style.left = `${pipePosition}px`;
 
-      mario.classList.remove('.jump')
-      mario.style.bottom = `${marioPosition}px`
+      mario.classList.remove('jump');
+      mario.style.bottom = `${marioPosition}px`;
 
-      mario.src = './src/img/game-over.png'
-      mario.style.width = '80px'
-      mario.style.marginLeft = '50px'
-      
-      
-      function stopAudioStart() {
-        audioStart.pause()
-      }
-      stopAudioStart()
-      
-      audioGameOver.play()
-      
-      function stopAudio() {
-        audioGameOver.pause()
-      }
-      setTimeout(stopAudio, 7000)
-      
-      gameOver.style.display = 'flex'
-      
-      clearInterval(loop)
+      mario.src = './src/img/game-over.png';
+      mario.style.width = '80px';
+      mario.style.marginLeft = '50px';
+
+      audioStart.pause();
+      audioGameOver.play();
+
+      gameOver.style.display = 'flex';
+
+      setTimeout(() => {
+        audioGameOver.pause();
+      }, 7000);
+
+      clearInterval(gameLoop); // Stop the loop
     }
-  }, 10)
-}
-
-loop()
+  }, 10);
+};
 
 document.addEventListener('keypress', e => {
-  const tecla = e.key
+  const tecla = e.key;
   if (tecla === ' ') {
-    jump()
+    jump();
   }
-})
+
+  if (tecla === 'Enter') {
+    startGame();
+  }
+});
 
 document.addEventListener('touchstart', e => {
   if (e.touches.length) {
-    jump() 
+    jump();
   }
-})
+});
 
-document.addEventListener('keypress', e => {
-  const tecla = e.key
-  if (tecla === 'Enter') {
-    startGame()
-  }
-})
